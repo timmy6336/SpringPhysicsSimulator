65aff33341e69bf04dc167196b53872d1cfe5892
timmy6336 <56135297+timmy6336@users.noreply.github.com>
2021-03-17 13:57:50 -0700

    Add files via upload

---
+++ spring.js
@@ -0,0 +1,38 @@
+class Spring
+{
+    constructor(game,k,x,a,b)
+    {
+        this.game = game;
+        this.k = k;
+        this.x = x;
+        this.a = a;
+        this.b = b;
+        this.distance = Math.sqrt(((this.b.x - this.a.x) * (this.b.x - this.a.x)) + ((this.b.y - this.a.y) * (this.b.y - this.a.y)));
+    }
+    update()
+    {
+        let dX = this.b.x - this.a.x;
+        let dY = this.b.y - this.a.y;
+        this.distance = Math.sqrt((dX * dX) + (dY * dY));
+        let force = (this.distance - this.x) * this.k;
+        let fX = (dX / this.distance) * force;
+        let fY = (dY / this.distance) * force;
+        this.a.applyForce(fX,fY);
+        this.b.applyForce(-fX,-fY);
+    }
+    draw(ctx)
+    {
+        var temp1 = ctx.lineWidth;
+        var temp2 = ctx.strokeStyle;
+
+        ctx.beginPath();
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = "White";
+        ctx.moveTo(this.a.x,this.a.y);
+        ctx.lineTo(this.b.x,this.b.y);
+        ctx.stroke();
+        ctx.lineWidth = temp1;
+        ctx.strokeStyle = temp2;
+        //console.log(this.distance);
+    }
+}
